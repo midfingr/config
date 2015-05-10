783e3e79cbd9dfd2090de43106ddac7c32c6c982
midfingr <3fingrs@gmail.com>
2015-05-10 23:30:33 +0000

    Remove duplicated directory

--- .config/google-chrome/Default/Extensions/cpecbmjeidppdiampimghndkikcmoadk/0.4.3_0/background.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    if (request.get_options) {
-      chrome.storage.sync.get(null, sendResponse);
-    } else {
-      sendResponse({});
-    }
-  }
-);
--- .config/google-chrome/Default/Extensions/cpecbmjeidppdiampimghndkikcmoadk/0.4.3_0/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-crxmake --pack-extension=$(dirname $(readlink -f $0))
